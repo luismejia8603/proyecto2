573cf37226a95fea7a61e5d08da31a11865320ea
luismmejia03 <128619347+luismmejia03@users.noreply.github.com>
2023-07-03 13:30:17 -0600

    ultima parte

--- Reservar.cpp
+++ Reservar.cpp
@@ -10,7 +10,7 @@ class Reservar
 public: Reservar(){}
 
     void iniciarReserva(string dia){
-        cout<<"que día quiere reservar: ";
+        
         
         string nombre;
         string fecha;
@@ -20,29 +20,50 @@ public: Reservar(){}
         string carrera;
         int cantidad;
 
+        do
+        {
+            cout<<"ingresar nombre"<<endl;
+            cin>>nombre;
+            if (nombre!= ("l1")&& nombre!=("l2")&& nombre!=("l3")&& nombre!=("l4")&& nombre!=("l5"))
+            {
+                cout<<"laboratorio no disponible"<<endl;
+            }
+            cout<<nombre<<endl;
+        } while (nombre!= "l1"&& nombre!= "l2"&& nombre!="l3"&& nombre!="l4"&& nombre!="l5");
         
-        cout<<"ingresar nombre"<<endl;
-        cin>>nombre;
+       
         cout<<"fecha que quiere reservar: "<<endl;
         cin>>fecha;
-        cout<<"hora de la reserva: "<<endl;
+        cout<<"hora de la reserva : "<<endl;
         cin>>hora;
         cout<<"proposito (interno o externo):  "<<endl;
         cin>>proposito;
         cout<<"Numero de carnet:  "<<endl;
         cin>>carnet;
-        cout<<"carrera que estudia:  "<<endl;
-        cin>>carrera;
-        cout<<"cantidad de personas que usaran el laboratorio: "<<endl;
-        cin>>cantidad;
+        
+         while (carrera!= "ingenieria informatica") {
+            cout<<"carrera que estudia:  "<<endl;
+            getline(cin, carrera);
+            if (carrera!= "ingenieria informatica")
+            {
+             cout<<"solo disponible para ingenieria informatica"<<endl;
+            }
+        }
+
+        do
+            {
+            cout<<"cantidad de personas que usaran el laboratorio: "<<endl;
+            cin>>cantidad;
+            if (cantidad>18)
+            {
+            cout<<"cantidad maxima 18";
+            }
+        }
+        while (cantidad>18);
         cin.ignore();
-        
-        
         ControlRegistros control(dia);
         control.registrarlaboratorio(nombre,fecha,hora,proposito,carnet,carrera,cantidad);
-
     }
 
-    
 };
 
--- project.cpp
+++ project.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include "Reservar.cpp"
 #include <string>
+#include "VerificarDisponibilidad.cpp"
+#include "CancelarReserva.cpp"
+#include <cstdio>
+
 
 using namespace std;
 
+string traerNombre(string line) {
+    string l1 = "l1";
+    string l2 = "l2";
+    string l3 = "l3";
+    string l4 = "l4";
+    string l5 = "l5";
+    if(line.find(l1) != string::npos) {
+                string nombre = l1;
+                return nombre;
+            }
+            if(line.find(l2) != string::npos) {
+                string nombre = l2;
+                return nombre;
+            }
+            if(line.find(l3) != string::npos) {
+                string nombre = l3;
+                return nombre;
+            }
+            if(line.find(l4) != std::string::npos) {
+                string nombre = l4;
+                return nombre;
+            }
+            if(line.find(l5) != std::string::npos) {
+                string nombre = l5;
+                return nombre;
+            }
+}
 
 int main(){
     int opcion;
@@ -17,8 +48,7 @@ int main(){
         cout<<"1. Disponibilidad de laboratorios"<<endl;
         cout<<"2. Reservar laboratorio"<<endl;
         cout<<"3. Cancelar reserva "<<endl;
-        cout<<"4. Generar archivo de reservas de la semana "<<endl;
-        cout<<"5. Registro del laboratorio mas utilizado"<<endl;
+        cout<<"4. Registro del laboratorio mas utilizado"<<endl;
 
         cout<<"seleccionar opcion: ";
         cin>>opcion;
@@ -32,48 +62,143 @@ int main(){
 
     switch (opcion)
     {
-    case 1: //disponibilidad//
+    case 1: {
+
+        string dia;
+        cout<<"dia de la semana que quiere verificar disponibilidad: "<<endl;
+        cin>>dia;
+
+        VerificarDisponibilidad disponibilidad(dia);
+        vector<string>datos=disponibilidad.leerRegistro();
+    for (const auto& line : datos) {
+        cout << line <<endl;
+    }
+        
+    }
         
         
         break;
 
     case 2: { //reservar laboratorio//
-
+        
         string dia;
+        do
+        {
         cout<<"digite el día que quiere reservar el laboratorio (lunes,martes,miercoles,jueves,viernes):  ";
         cin>>dia;
+
+        if ((dia=="sabado")||(dia=="domingo"))
+        {
+            cout<<"Dias no disponibles."<<endl;
+        }
+        
+        
+
+        } while ((dia=="sabado")||(dia=="domingo"));
+        
 
         Reservar reservar;
         reservar.iniciarReserva(dia);
         
+        
+
         break;
     }
 
     case 3: { //cancelar reserva//
 
+        string dia;
+        string fecha;
+        string carnet;
+
+        cout<<"dia de la reserva: "<<endl;
+        cin>>dia;
+
+        cout<<"fecha: "<<endl;
+        cin>>fecha;
+
+        cout<<"carnet: "<<endl;
+        cin>>carnet;
+
+        CancelarReserva cancelarReserva;
+        cancelarReserva.borrarLinea(dia,fecha,carnet);
+
         
 
 
         break;
     }
 
-    case 4: //Archivo reservas de la semana//
+    case 4: //laboratorio mas usado//
+        {
+        vector<string> nombres;
+        VerificarDisponibilidad disponibilidadLunes("lunes");
+        vector<string>datos1=disponibilidadLunes.leerRegistro();
+        for (const auto& line : datos1) {
+            cout << line <<endl;
+            string nombre = traerNombre(line);
+            cout << nombre <<endl;
+            nombres.push_back(nombre);
+        }
+        VerificarDisponibilidad disponibilidadMartes("martes");
+        vector<string>datos2=disponibilidadMartes.leerRegistro();
+        for (const auto& line : datos2) {
+            cout << line <<endl;
+            string nombre = traerNombre(line);
+            cout << nombre <<endl;
+            nombres.push_back(nombre);
+        }
+        VerificarDisponibilidad disponibilidadMiercoles("miercoles");
+        vector<string>datos3=disponibilidadMiercoles.leerRegistro();
+        for (const auto& line : datos3) {
+            cout << line <<endl;
+            string nombre = traerNombre(line);
+            cout << nombre <<endl;
+            nombres.push_back(nombre);
+        }
+        VerificarDisponibilidad disponibilidadJueves("jueves");
+        vector<string>datos4=disponibilidadJueves.leerRegistro();
+        for (const auto& line : datos4) {
+            cout << line <<endl;
+            string nombre = traerNombre(line);
+            cout << nombre <<endl;
+            nombres.push_back(nombre);
+        }
+        VerificarDisponibilidad disponibilidadViernes("viernes");
+        vector<string>datos5=disponibilidadViernes.leerRegistro();
+        for (const auto& line : datos5) {
+            cout << line <<endl;
+            string nombre = traerNombre(line);
+            cout << nombre <<endl;
+            nombres.push_back(nombre);
+        }
+        cout<<"TOTAL"<<endl;
+        for (const auto& nombre : nombres) {
+            cout << nombre <<endl;
+        }
 
         break;
-
-    case 5: //laboratorio mas usado//
-
-        break;
-    
+        }
     default:
         break;
+
+        system("cls");
     }
 
     cout<<"Salir? "<<endl;
     cin>>resp;
 
+    system("cls");
+
     } while (resp == "no");
+
+   
 
 
     return 0;
+}
+
+string getFieldValue(string input) {
+    
+    return "";
 }
